237161f68f8b561ac2c94a0e01c03efb76d6a752
ttv-dev-spirit <ttv.dev.spirit@gmail.com>
2023-01-02 17:37:26 +0700

    Added plugins directly to project

---
+++ Assets/Plugins/Asset Usage Finder/Editor/Data/SearchTarget.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using static AssetUsageFinder.PrefabUtilities;
+using Object = UnityEngine.Object;
+
+namespace AssetUsageFinder {
+	[Serializable]
+	class SearchTarget {
+		public Object Target;
+		public Option<Object[]> Nested;
+		public Object Root;
+		public Scene Scene;
+		public UnityEditor.SceneManagement.PrefabStage Stage;
+
+		public SearchTarget(Object target, FindModeEnum findMode, string sceneOrStagePath = null) {
+			Asr.IsNotNull(target, "Asset you're trying to search is corrupted");
+			Target = target;
+			var path = sceneOrStagePath ?? AssetDatabase.GetAssetPath(Target);
+			switch (findMode) {
+				case FindModeEnum.File:
+					Asr.IsTrue(string.IsNullOrEmpty(sceneOrStagePath));
+					Root = AssetDatabase.LoadMainAssetAtPath(path);
+					Nested = AufUtils.LoadAllAssetsAtPath(path);
+					break;
+				case FindModeEnum.Scene:
+				case FindModeEnum.Stage:
+					Root = Target;
+					if (Target is GameObject go) {
+						Nested = go.GetComponents<Component>().OfType<Object>().ToArray();
+						Stage = UnityEditor.SceneManagement.PrefabStageUtility.GetCurrentPrefabStage();
+						if (findMode == FindModeEnum.Scene) {
+							if (string.IsNullOrEmpty(sceneOrStagePath))
+								sceneOrStagePath = go.scene.path;
+							Scene = SceneManager.GetSceneByPath(sceneOrStagePath);
+						}
+					}
+					else if (Target is Component c) {
+						Nested = default;
+						if (findMode == FindModeEnum.Scene) {
+							if (string.IsNullOrEmpty(sceneOrStagePath))
+								sceneOrStagePath = c.gameObject.scene.path;
+							Scene = SceneManager.GetSceneByPath(sceneOrStagePath);
+						}
+					}
+					else {
+						Nested = default;
+					}
+					break;
+			}
+		}
+
+		public bool Check(Object t) {
+			var tt = Target == t;
+			return t && Nested.TryGet(out var n) && n.Aggregate(tt, (current, o) => current || o == t);
+		}
+	}
+}
---
+++ Assets/Plugins/Asset Usage Finder/readme.txt
@@ -0,0 +1,13 @@
+Features in v4.12:
+1) Improved work with cache persistence
+2) Fixed minor component search bugs
+3) 2018.4+ version support added
+4) Fixed Usages search edgecase bug - When target is a component of prefab instance in scene 
+
+Features in v4.0:
+
+1) Full Unity 2018+ compatibility - Nested prefabs support added
+2) Search for AnimatorController states
+3) Open Scene search
+4) Minor UX and functional improvements
+5) User Support boosted! Support via messenger (get an answer under 12 hours) - Telegram channel https://t.me/joinchat/BdWObFckBlxZOW06Unnv6w
